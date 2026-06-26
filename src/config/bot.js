203549a1df84ac59f8b656f984ffb1907b75f0b6
paulalainvillaozuna-hash <paulalainvillaozuna@gmail.com>
2026-06-26 15:26:22 -0700

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Organizando la store CSK",
         // Activity type number (0 = Playing).
-        type: 4,
+        type: 3,
       },
     ],
   },
